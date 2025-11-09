80d8db664bbd2694e89d288545ef7ea13ab9713d
hicorelearningapp <hicorelearningapp@gmail.com>
2025-11-09 12:24:25 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navbar />} />
       </Routes>
     </Router>
   );
